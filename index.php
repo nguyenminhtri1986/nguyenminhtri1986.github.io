590727cd24f45332867ec72baa3c2158b391ab96
nguyenminhtri1986 <nguyenminhtri1986@gmail.com>
2023-03-23 13:37:12 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+    <?php
+        echo 'chao mung ban den php';
+    ?>
+</body>
+</html>
